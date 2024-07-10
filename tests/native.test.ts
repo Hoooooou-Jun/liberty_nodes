12dae1cfbd0e4c24895a0bb2b5a8f81c64d1f635
Hoooooou-Jun <jipkim2@gmail.com>
2024-07-10 18:46:38 +0900

    build set

--- tests/native.test.ts
+++ tests/native.test.ts
@@ -1,28 +1,15 @@
-u// Manually initialize variables that are automatically defined in Playground
-const PROGRAM_ID = new web3.PublicKey("HGWnztHyhBVRAghFkH2iFWgRE3XpUJa8uxAbrdcAC1yt");
-const connection = new web3.Connection("https://api.devnet.solana.com", "confirmed");
-const wallet = { keypair: web3.Keypair.generate() };
+import * as web3 from "@solana/web3.js";
+const PROGRAM_ID = new web3.PublicKey(
+  "8Jary8qeULbgUf181XGYpigZzCAyjwpv7NN7qQy1bKMF"
+);
+const connection = new web3.Connection(
+  "http://localhost:8899",
+  "confirmed"
+);
 
-se solana_program::{
-	account_info::AccountInfo,
-	entrypoint,
-	entrypoint::ProgramResult,
-	pubkey::Pubkey,
-};
-
-mod state;
-mod instruction {
-	pub mod create_cell;
-}
-
-use crate::instruction::create_cell::create_cell;
-
-entrypoint!(process_instruction);
-
-pub fn process_instruction(
-	program_id: &Pubkey,
-	accounts: &[AccountInfo],
-	instruction_data: &[u8],
-) -> ProgramResult {
-	create_cell(program_id, accounts, instruction_data)
-}
+describe("Test", () => {
+  it("Create comment", async () => {
+    const commentKeypair = new web3.Keypair();
+    console.log(connection);
+  });
+});
